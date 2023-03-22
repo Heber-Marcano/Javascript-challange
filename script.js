00aee45a279dd945fb4b1b4b99852f5f4ea17c3c
Heber <hebelioo@gmail.com>
2023-03-22 14:29:54 -0400

    sarted generate function

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ let numbers = [1,2,3,4,5,6,7,8,9,0];
 let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let alphabetUpperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 
+function generatePassword() {
+  
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
