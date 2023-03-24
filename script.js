a23d13a48c04fec1ff918af598d51b8c19068e83
Heber <hebelioo@gmail.com>
2023-03-23 22:44:11 -0400

    change comments

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-// 
+
 let specialChar = ["!", ".", "/", "$", "%", "^", "&", "*", "<", ">", "-", "+", ","];
 let numbers = [1,2,3,4,5,6,7,8,9,0];
 let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
